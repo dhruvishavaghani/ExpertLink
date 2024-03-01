729ff85c27a55d448787bbcb0933d604b83b8670
Dhruvisha Vaghani <dhruvishavaghani0509@gmail.com>
2024-03-01 18:31:44 +0530

    Basic Changes + Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
 import Home from './Pages/Home'
+import { createContext, useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,Routes,Route,
+} from "react-router-dom";
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+
 
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+    <Router>
+      <Nav/>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   )
 }
 
